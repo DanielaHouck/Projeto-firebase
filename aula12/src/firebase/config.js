bd50561761c20299b5e4e13db0c8bbbd460bfd95
DanielaHouck <daniela_houck@hotmail.com>
2022-10-27 09:38:25 -0300

    1 commit

---
+++ aula12/src/firebase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDhF_Mnmz0KIrsWvooJESxJvcgRbrs0Khg",
+  authDomain: "labwebdsmdani.firebaseapp.com",
+  projectId: "labwebdsmdani",
+  storageBucket: "labwebdsmdani.appspot.com",
+  messagingSenderId: "999419359284",
+  appId: "1:999419359284:web:e4b0691a213d23edbd5a61"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
